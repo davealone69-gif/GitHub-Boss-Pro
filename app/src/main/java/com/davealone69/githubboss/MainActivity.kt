a0d9ab52965393ad3e545ed793ed798d2098e3f9
davealone69-gif <davealone69@gmail.com>
2026-08-17 01:28:24 +0800

    fix(ui): launch completed GitHub Boss system UI

--- app/src/main/java/com/davealone69/githubboss/MainActivity.kt
+++ app/src/main/java/com/davealone69/githubboss/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.davealone69.githubboss.ui.GitHubBossApp
 import com.davealone69.githubboss.ui.GitHubViewModel
 
@@ -12,7 +13,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val viewModel = GitHubViewModel(application)
-        setContent { MaterialTheme { GitHubBossApp(viewModel) } }
+        setContent {
+            MaterialTheme {
+                GitHubBossApp(viewModel<GitHubViewModel>())
+            }
+        }
     }
 }
